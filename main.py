976223f68ee29935586a0fe98e47ab2e4c8a233f
Hance Wu <32484940+wzh4464@users.noreply.github.com>
2023-04-14 19:43:44 +0800

    success
    Signed-off-by: Hance Wu <32484940+wzh4464@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,41 +2,62 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-
-def get_sub_conference_urls(conference_url):
-    response = requests.get(conference_url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    urls = [a["href"]
-            for a in soup.find_all("a", href=True) if "conf" in a["href"]]
-    return urls
-
-
 # 读取csv文件并存储为pandas dataframe
 df = pd.read_csv('conferences.csv')
 
-# 遍历每一行，爬取对应网址的页面，并获取包含关键词的论文标题
-for index, row in df.iterrows():
-    # 获取会议简称和网址
-    conf_abbr = row['abbreviation']
-    conf_url = row['url']
+# 提取df中url
+urls = df['url'].values
 
-    # 获取主会议页面上所有子会议页面的链接
-    sub_urls = get_sub_conference_urls(conf_url)
+# url 按 ‘/’ 分割，取 db 后的两个元素
+# 例如：https://dblp.org/db/conf/icml/icml2020.html
+# 则取 conf 和 icml
 
-    # 遍历每个子会议页面
-    for sub_url in sub_urls:
-        # 发送HTTP请求获取网页内容
-        response = requests.get(sub_url)
-        # 解析HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
+select_conf = []
 
-        # 在XML文档中查找所有包含关键词的标题，并保存到列表中
-        titles = []
-        for hit in soup.find_all('hit'):
-            title = hit.find('title')
-            if title and 'unlearn' in title.text.lower():
-                titles.append(title.text.strip())
+for url in urls:
+    url_parts = url.split('/')
+    for part in url_parts:
+        if part == 'db':
+            conf_abbr = url_parts[url_parts.index(part) + 1]
+            subconf_abbr = url_parts[url_parts.index(part) + 2]
+            select_conf.append([conf_abbr, subconf_abbr])
+            break
 
-        # 如果找到了包含关键词的标题，打印出来
-        if len(titles) > 0:
-            print(f"{conf_abbr}: {sub_url}: {', '.join(titles)}")
+
+
+result = []
+
+# for hit in data['result']['hits']['hit']:
+#     title = hit['info']['title']
+#     conf = hit['info']['venue']
+#     if keywords in title.lower():
+#         result.append([title, conf])
+
+# save df to a result.csv
+# df = pd.DataFrame(result, columns=['title', 'conf'])
+# df.to_csv('result.csv', index=False)
+# 遍历每一行，爬取对应网址的页面，并获取包含关键词的论文标题、摘要和关键词
+
+
+# 限制搜索范围
+search_url = f"https://dblp.org/search/publ/api?q=unlearn&h=1000&f=abstracts&f=title&f=keywords&format=json"
+
+# 发送HTTP请求获取搜索结果
+response = requests.get(search_url)
+# 解析JSON格式的搜索结果
+result_json = response.json()
+# 遍历搜索结果中的每篇论文
+for publ in result_json['result']['hits']['hit']:
+    # 获取论文信息
+    title = publ['info']['title']
+    key_parts = publ['info']['key'].split('/')
+    conf_abbr = key_parts[0]
+    subconf_abbr = key_parts[1]
+    # if match select conf
+    if [conf_abbr, subconf_abbr] in select_conf:
+        result.append([title, conf_abbr, subconf_abbr])
+
+# save result to a result.csv
+result_df = pd.DataFrame(result, columns=['title', 'conf_abbr', 'subconf_abbr'])
+result_df.to_csv('result.csv', index=False)
+        
